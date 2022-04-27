c21b311cda40c3eb848b6f63700462ff11830f5f
AkH <akh.buroscope@gmail.com>
2022-04-27 19:03:57 +0200

    JS : refactorisation

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
+const searchBar = document.querySelector('.searchBar');
+const btnComboboxContainer = document.querySelector('#dropdown-input');
+const inputIngredients = document.querySelector('#input-ingredients');
+const btnIngredients = document.querySelector('#btn-ingredients');
+const ingredientsList = document.querySelector('#ingredients-list');
+const recipesList = document.querySelector('#recipes-list');
 
-
-// create tag
 function createTag(currentValue, type) {
-    console.log(type)
     const tagSection = document.querySelector('#tags');
     const tag = document.createElement('li');
     tag.classList.add('tag')
@@ -16,7 +19,8 @@ function createTag(currentValue, type) {
         case 'ustensil':
             tag.classList.add('ustensil')
             break;
-        default: tag.classList.add('ingredient')
+        default:
+            tag.classList.add('ingredient')
             break;
     }
     tag.textContent = currentValue
@@ -25,172 +29,34 @@ function createTag(currentValue, type) {
     })
     tagSection.appendChild(tag);
 }
- 
-// display ingredients
-async function displayIngredients(recipes) {
-    // select DOM
-    const ingredientsSection = document.querySelector('#ingredients-list');
-    // get all ingredients;
-    const arrAllIngredients = [];
-    recipes.forEach(recipe => {
-        const ingredient = recipeFactory(recipe).getIngredient();
-        ingredient.forEach((el) => arrAllIngredients.push(el))
-    });
-    // return once, only unique data
-    const uniqueIngredient = arrAllIngredients.filter((el, idx) => {
-        return arrAllIngredients.indexOf(el) === idx
-    });
 
-    // insert DOM
-    const ingredientItem = uniqueIngredient.map(el => {
-        return `<li class="ingredient-tag">${el}</li>`
-    }).join('');
-    ingredientsSection.insertAdjacentHTML('afterBegin', ingredientItem);
+function 
+
+
+const displayIngredients = (recipes) => {
+    const arr = [];
+    recipes.forEach((recipe) => {
+            recipe.ingredients.map(el => {
+                const cleanAccentValue = el.ingredient.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+                arr.push(cleanAccentValue)
+            });
+        });
+    const uniqueValue = [...new Set(arr)].sort();
+
+    const htmlString = uniqueValue
+        .map((el) => {
+                return `<li class="ingredient-tag">${el}</li>`
+        }).join('');
+
+    ingredientsList.innerHTML = htmlString;
+
     const ingredients = document.querySelectorAll('.ingredient-tag');
-    ingredients.forEach((el)=> el.addEventListener('click', () =>{
+    ingredients.forEach((el) => el.addEventListener('click', () => {
         createTag(el.textContent, 'ingredient')
     }));
 }
 
-// display appliances
-async function displayAppliances(recipes) {
-    // select DOM
-    const appliancesSection = document.querySelector('#appliances-list');
-    // get all appliances;
-    const arrAllAppliances = [];
-    recipes.forEach(recipe => {
-        const appliance = recipeFactory(recipe).getAppliance();
-        arrAllAppliances.push(appliance);
-    });
-
-    // return once, only unique data
-    const uniqueAppliance = arrAllAppliances.filter((el, idx) => {
-        return arrAllAppliances.indexOf(el) === idx
-    });
-
-    // insert DOM
-    const applianceItem = uniqueAppliance.map(el => {
-        return `<li class="appliance-tag">${el}</li>`
-    }).join('');
-    appliancesSection.insertAdjacentHTML('afterBegin', applianceItem);
-    const appliances = document.querySelectorAll('.appliance-tag');
-    appliances.forEach((el)=> el.addEventListener('click', () =>{
-        createTag(el.textContent, 'appliance')
-    }));
-}
-
-// display ustensils
-async function displayUstensils(recipes) {
-    // select DOM
-    const ustensilsSection = document.querySelector('#ustensils-list');
-    // get all ustensils
-    const arrAllUstensils = [];
-    recipes.forEach(recipe => {
-        const ustensil = recipeFactory(recipe).getUstensil();
-        arrAllUstensils.push(ustensil);
-    });
-
-    // flatten the array
-    const flatArrUstensils = arrAllUstensils.flat();
-    // console.log(flatArrUstensils)
-
-    // return once, only unique data
-    const uniqueUstensils = flatArrUstensils.filter((el, idx) => {
-        return flatArrUstensils.indexOf(el) === idx
-    });
-
-    // insert DOM
-    const ustensilItem = uniqueUstensils.map(el => {
-        return `<li class="ustensil-tag">${el}</li>`
-    }).join('');
-
-    ustensilsSection.insertAdjacentHTML('afterBegin', ustensilItem);
-    const ustensils = document.querySelectorAll('.ustensil-tag');
-    ustensils.forEach((el)=> el.addEventListener('click', () =>{
-        createTag(el.textContent, 'ustensil')
-    }));
-}
-
-// display recipes
-async function displayRecipes(recipes) {
-    const recipesSection = document.querySelector('#recipes-list');
-    const arrAllRecipes = [];
-    recipes.forEach(recipe => {
-        const recipesCard = recipeFactory(recipe).getArticle();
-        arrAllRecipes.push(recipesCard);
-    });
-
-    const recipesItem = arrAllRecipes.map(el => {
-        return `<li>${el}</li>`
-    }).join('')
-    recipesSection.insertAdjacentHTML('afterBegin', recipesItem);
-}
-
-// initialisation
-function init() {
-    displayIngredients(recipes);
-    displayAppliances(recipes);
-    displayUstensils(recipes);
-    displayRecipes(recipes);
-}
-init();
-
-
-
-//searchBar listener
-const searchBarInput = document.querySelector('.searchBar');
-
-searchBarInput.addEventListener('input', function (e) {
-    e.preventDefault();
-    const currentValue = (e.target.value).toLowerCase();
-
-    if (currentValue.length >= 3) {
-        const recipesList = document.querySelectorAll('article');
-        const currentValue = (e.target.value).toLowerCase();
-        // console.log(currentValue);
-
-        [...recipesList].filter((el, idx) => {
-            // console.log(el, idx)
-        
-            // filter by title
-            const titleWord = el.querySelector('h2').textContent.toLowerCase();
-
-            // filter by ingredients
-            const ingredientsList = el.querySelectorAll('.ingredient-item');
-            const arrIngredients = [];
-            ingredientsList.forEach((el) => arrIngredients.push(el.textContent.toLowerCase().split(':')[0].trim()))
-            const matchValue = (elt) => elt.includes(currentValue);
-
-            // filter by descriptions
-            const descriptionWord = el.querySelector('.description').textContent.toLowerCase();
-
-            // style result
-            (titleWord.includes(currentValue) || arrIngredients.some(matchValue) || descriptionWord.includes(currentValue)) ?
-            recipesList[idx].parentElement.style.display = 'block' :
-            recipesList[idx].parentElement.style.display = 'none'
-        });
-    }
-});
-
-searchBarInput.addEventListener('keyup', function (e) {
-    e.preventDefault();
-    const form = document.querySelector('form');
-    const currentValue = (e.target.value).toLowerCase();
-
-    // tag creation & remove
-    if (((e.key || e.code) === ('Enter' || 13)) && (currentValue.length >= 3)) {
-        createTag(currentValue);
-        form.reset();
-    }
-});
-
-
 // input style change onclick
-const btnComboboxContainer = document.querySelector('#dropdown-input');
-const btnIngredients = document.querySelector('#btn-ingredients');
-const inputIngredients = document.querySelector('#input-ingredients');
-const ingredientsList = document.querySelector('.dropdown-menu');
-
 btnIngredients.addEventListener('click', () => {
 
     if (btnIngredients.classList.contains('btn-input-show')) {
@@ -212,6 +78,42 @@ btnIngredients.addEventListener('click', () => {
 
 });
 
-// inputIngredients.addEventListener('focus', () => {
-//     ingredientsList.classList.add('.dropdown-menu.show');
-// })
+
+const displayRecipes = (recipes) => {
+    const htmlString = recipes
+        .map((recipe) => {
+            const cards = recipeFactory(recipe).getArticle();
+            const recipeCard = cards.article;
+            return recipeCard
+        })
+        .join('');
+    recipesList.innerHTML = htmlString;
+};
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    const currentValueSize = e.target.value.length;
+
+    if(currentValueSize >= 3) {
+        const filteredRecipes = recipes.filter((recipe) => {
+            return (
+                recipe.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').includes(searchString) ||
+                recipe.ingredients.some(el => {
+                    return el.ingredient.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').includes(searchString);
+                }) ||
+                recipe.description.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').includes(searchString)
+            );
+        });
+        displayRecipes(filteredRecipes);
+    } else {
+        displayRecipes(recipes);
+    }
+    
+});
+
+
+function init() {
+    displayIngredients(recipes)
+    displayRecipes(recipes);
+};
+init();
--- scripts/recipe-factory.js
+++ scripts/recipe-factory.js
@@ -11,7 +11,7 @@ function recipeFactory(datas) {
         ustensils
     } = datas;
 
-
+// console.log(datas)
 
     function getIngredient() {
         const arrIngredients = [];
@@ -37,25 +37,62 @@ function recipeFactory(datas) {
     }
 
     function getArticle() {
-        const article = `
-        <article class="card shadow-sm p-0 border-0 mx-auto mb-5" data-id=${id}>
-             <img class="card-img-top" src="..." alt=" ">
-            <div class="card-body">
-                <header class="d-flex justify-content-between align-items-center mb-2">
-                    <h2 class="card-title mb-0">${name}</h2>
-                    <span class="fw-bolder"><img src="./assets/watch.svg" alt="" class="time">${time} mins</span>
-                </header>
-                <div class="d-flex flex-column flex-md-row justify-content-between align-items-start card-description">
-                     <ul class="list-unstyled">${ ingredients.map(el => {
-                        return `<li><span class="fw-bolder ingredient-item">${el.ingredient} :</span> ${el.quantity ? el.quantity : ''} ${el.unit ? el.unit : ''}</li>`
-                    }).join('')}</ul>
-                    <p class="card-text h-100 ml-2-md description">${description}</p>
-                </div>
-            </div>
-        </article>
-        `;
-        return article;
+
+            const articleRaw = 
+            `
+                <li class="article-container">
+                    <article class="card shadow-sm p-0 border-0 mx-auto mb-5" data-id=${id}>
+                            <img class="card-img-top" src="" alt=" ">
+                        <div class="card-body">
+                            <header class="d-flex justify-content-between align-items-center mb-2">
+                                <h2 class="card-title mb-0">${name}</h2>
+                                <span class="fw-bolder"><img src="./assets/watch.svg" alt="" class="time">${time} mins</span>
+                            </header>
+                            <div class="d-flex flex-column flex-md-row justify-content-between align-items-start card-description">
+                                    <ul class="list-unstyled">${ ingredients.map(el => {
+                                    return `<li><span class="fw-bolder ingredient-item">${el.ingredient} :</span> ${el.quantity ? el.quantity : ''} ${el.unit ? el.unit : ''}</li>`
+                                }).join('')}</ul>
+                                <p class="card-text h-100 ml-2-md description">${description}</p>
+                            </div>
+                        </div>
+                    </article>
+               </li>
+            `
+            const arr = articleRaw.split('\n');
+            const arrTrim = arr.map((el) => el.trim());
+            const articleClean = arrTrim.join('');
+
+            const ingredientsClean = [];
+            ingredients.map(el => {
+                ingredientsClean.push(el.ingredient);
+            });
+
+
+            const ustensilsClean = [];
+            ustensils.map(el => {
+                ustensilsClean.push(el);
+            });
+
+
+            const appliancesClean = appliance;
+
+
+
+            class Article {
+                constructor(id, article, ingredients, ustensils, appliances) {
+                    this.id = id;
+                    this.article = article;
+                    this.ingredients = ingredients;
+                    this.ustensils = ustensils;
+                    this.appliances = appliances;
+                }
+            }
+        
+            const article = new Article(id, articleClean, ingredientsClean, ustensilsClean, appliancesClean);
+            return article;
+
     }
+
 
 
     return {
