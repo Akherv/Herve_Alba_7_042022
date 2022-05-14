02c80c2d8bf14f75fbac40fd0a4829eae04b32e7
AkH <akh.buroscope@gmail.com>
2022-05-14 17:07:16 +0200

    refactorisation

--- scripts/searchAlgos/refreshAll.js
+++ scripts/searchAlgos/refreshAll.js
@@ -1,8 +1,13 @@
 import createSearchValuesObj from '../factories/searchValuesObj-factory.js';
 import { checkIndividualConditions } from './conditions.js';
 
+// manage the I/O of the global state "arrSearchValues" & fire the searchValueObj-factory to structure the array on creation of a new obj
 const refreshArrSearchValues = (searchValue, type, arrSearchValues) => {
 
+    // case : type === 'searchBar' 
+    // searchValue >= 3 & (tags = 0) => create a new searchValObj of type 'searchBar'
+    //                  & (tags > 0) => tag type 'searchBar' ? (replace searchValObj of type 'searchBar' with the current val) : (create a new searchValObj of type 'searchBar')
+    // searchValue < 3 & (tags >= 0) => tag type 'searchBar' ? remove tag of type 'searchBar'
     if (type === 'searchBar') {
         if (searchValue.length >= 3) {
             if (arrSearchValues.length === 0) {
@@ -30,7 +35,9 @@ const refreshArrSearchValues = (searchValue, type, arrSearchValues) => {
         }
     }
 
-
+    // case : other type
+    // searchValue >= 3 & (tags > 0) => if there is a tag with the same name ? (replace this searchValObj with the current val) : (create a new searchValObj of this type)
+    //                  & (tags = 0) => create a new searchValObj of this type
     if (type !== 'searchBar' && searchValue.length >= 3) {
         const res = arrSearchValues.some((el) => el.name.includes(searchValue));
         if (arrSearchValues.length > 0) {
@@ -43,12 +50,14 @@ const refreshArrSearchValues = (searchValue, type, arrSearchValues) => {
             } else {
                 arrSearchValues.push(createSearchValuesObj(searchValue, type));
             }
-        } else {
+        } 
+        else {
             arrSearchValues.push(createSearchValuesObj(searchValue, type));
         }
     }
 }
 
+// filter each recipe which match the global check of all the individuals conditions given by type of object which are in the global state "arrSearchValues" at this moment
 const refreshArrFilteredRecipes = (arrAllRecipes, arrSearchValues) => {
     const refreshRecipes = arrAllRecipes.filter(recipe => {
         const res = checkIndividualConditions(recipe, arrSearchValues);
